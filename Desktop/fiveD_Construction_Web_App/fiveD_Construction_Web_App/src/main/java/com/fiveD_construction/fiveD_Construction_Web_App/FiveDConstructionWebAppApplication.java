5fc2964b6205cd528d3fc3f8026b8b0d8955553d
Rushi <dholerushi.com@gmail.com>
2025-05-21 14:54:34 +0530

    Created Spring MVC project for client (web application)

---
+++ Desktop/fiveD_Construction_Web_App/fiveD_Construction_Web_App/src/main/java/com/fiveD_construction/fiveD_Construction_Web_App/FiveDConstructionWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.fiveD_construction.fiveD_Construction_Web_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiveDConstructionWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiveDConstructionWebAppApplication.class, args);
+	}
+
+}
---
+++ Desktop/fiveD_Construction_Web_App/fiveD_Construction_Web_App/src/test/java/com/fiveD_construction/fiveD_Construction_Web_App/FiveDConstructionWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiveD_construction.fiveD_Construction_Web_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiveDConstructionWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
